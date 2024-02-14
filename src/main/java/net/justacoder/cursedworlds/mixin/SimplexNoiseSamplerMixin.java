e9eab04a8be376bf3b3f40bb17c0c5e414aba5f6
davidsaltacc <d.lumpi@outlook.de>
2024-02-14 15:38:47 +0100

    version increase to 0.11

--- src/main/java/net/justacoder/cursedworlds/mixin/SimplexNoiseSamplerMixin.java
+++ src/main/java/net/justacoder/cursedworlds/mixin/SimplexNoiseSamplerMixin.java
@@ -2,9 +2,7 @@ package net.justacoder.cursedworlds.mixin;
 
 import net.justacoder.cursedworlds.ModifiedNoiseFunctions;
 import net.minecraft.util.math.noise.SimplexNoiseSampler;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
